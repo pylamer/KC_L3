3ec37e751dc77b61cb4a3272e23267a3deb55feb
Valeriy <knyazvs85@gmail.com>
2023-05-13 18:22:40 +0300

    some commit

--- Dockerfile
+++ Dockerfile
@@ -3,6 +3,5 @@ WORKDIR /app
 COPY requirements.txt requirements.txt
 RUN pip install --upgrade pip && pip install -r requirements.txt
 COPY main.py main.py
-COPY wsgi.py wsgi.py
 EXPOSE 7500
-CMD ["gunicorn", "--bind", "0.0.0.0:7500", "--workers=10", "wsgi:app"]
+CMD ["gunicorn", "--bind", "0.0.0.0:7500", "--workers=10", "main:app"]
--- main.py
+++ main.py
@@ -1,12 +1,43 @@
 from flask import Flask, jsonify, make_response
 import time
+import json
+import requests
 
 app = Flask(__name__)
+URL_SEC_NUMBER = 'https://lab.karpov.courses/hardml-api/module-5/get_secret_number'
 
 @app.route('/')
 def response():
     time.sleep(1)
     return make_response(jsonify({"response": "gunicorn reply"}), 200)
 
+@app.route('/return_secret_number')
+def return_secret_number():
+    time.sleep(1)
+    return jsonify(secret_number=secret_number)
+
+def get_secret_number(url):
+    flag = True
+    count = 1
+    secret_number = None
+    while flag and count < 100:
+        time.sleep(0.5)
+        # print(count)
+        try:
+            response = requests.get(URL_SEC_NUMBER).text.replace("'", '"')
+            res = json.loads(response)
+            secret_number = res['secret_number']
+            flag = False
+        except:
+            count += 1
+
+    if secret_number is None:
+        return -1
+    else:
+        return secret_number
+
+if __name__ == '__main__':
+    secret_number = get_secret_number(URL_SEC_NUMBER)
+    app.run()
 
 
